887a25531975b53758fe05fda3b3d72f6ee3e437
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-04-01 21:34:08 +0600

    static roghly done

--- src/components/CardSection/CardSection.jsx
+++ src/components/CardSection/CardSection.jsx
@@ -11,7 +11,7 @@ const CardSection = ({ toolsPromise, cart, setCart }) => {
         setAllTools(toolsData)
     }, [toolsData])
 
-    console.log(allTools)
+    // console.log(allTools)
 
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
